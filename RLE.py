b251fa363f01b351218a54cfe0de758ce611f379
a-hamshary <ahmad.elhamshary@gmail.com>
2020-11-24 21:20:53 +0200

    1. Added the toStrV function which works perfectly. 2. I discovered that the toStrH was in fact calculating the vertical run length encoding so I swapped names with the toStrV

--- RLE.py
+++ RLE.py
@@ -12,12 +12,12 @@ def imOpen(input_img):
     #plt.imshow(im)
     #plt.show()
 
-def toStrH(img_obj):
+def toStrV(img_obj):
     width, height = img_obj.size
     print(width, height)
     np_img = np.array(img_obj).flatten()
 
-    horizontal_str = "H "
+    horizontal_str = "V "
     horizontal_str += str(width)
     horizontal_str += " "
     horizontal_str += str(height)
@@ -31,7 +31,7 @@ def toStrH(img_obj):
 
     for i in range(0, width):
         for j in range(0, height):
-            if j % width == 0:
+            if j % width-1 == 0:
                 horizontal_str += "\n"
             if np_img[j] != pixel_value:
                 horizontal_str += "PV"
@@ -48,7 +48,46 @@ def toStrH(img_obj):
                 first_element = j
             last_element = j
 
-    print(horizontal_str)
+    # print(horizontal_str)
+    return horizontal_str
+
+
+def toStrH(img_obj):
+    width, height = img_obj.size
+    print(width, height)
+    np_img = np.array(img_obj).flatten()
+
+    horizontal_str = "H "
+    horizontal_str += str(width)
+    horizontal_str += " "
+    horizontal_str += str(height)
+    horizontal_str += "\n"
+
+    pixel_value = np_img[0]
+    first_element = 0
+    last_element = 0
+
+    for i in range(0, height):
+        for j in range(0, width):
+            if j % width-1 == 0:
+                horizontal_str += "\n"
+            if np_img[j] != pixel_value:
+                horizontal_str += "PV"
+                horizontal_str += str(pixel_value)
+                horizontal_str += " "
+                horizontal_str += "F"
+                horizontal_str += str(first_element)
+                horizontal_str += " "
+                horizontal_str += "L"
+                horizontal_str += str(last_element)
+                horizontal_str += " "
+
+                pixel_value = np_img[j]
+                first_element = j
+            last_element = j
+
+    # print(horizontal_str)
+    return horizontal_str
 
 def toFile(text, file):
     thisfile = open(file, "w")
@@ -59,4 +98,8 @@ def toFile(text, file):
 
 
 a = imOpen("Bars.bmp")
-toStrH(a)
+s = toStrV(a)
+toFile(s, "Bars_V.txt")
+
+x = toStrH(a)
+toFile(x,"Bars_H.txt" )
